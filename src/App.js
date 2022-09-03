452929c2e6153e6ca4371fef9b09076984e62ba7
MaheshTiwaree <mahesh.tiwari@bugendaitech.com>
2022-09-03 19:40:12 +0530

    Added auction table

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './screens/home/Home';
+import Navbar from './components/layout/navbar/Navbar';
+import Footer from './components/layout/footer/Footer';
+import Auction from './screens/auction/Auction';
 
 function App() {
   return (
     <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/auction' element={<Auction />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
